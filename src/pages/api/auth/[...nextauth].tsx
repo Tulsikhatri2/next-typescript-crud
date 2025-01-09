2b4c89d111cb79164610162b82bf191442251946
Tulsi Khatri <tulsiyuvasoft447@gmail.com>
2025-01-09 15:46:19 +0530

    09/01/2025

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
     providers:[
         GoogleProvider({
-            clientId:"502531248944-geqqani0quqrv51qkjsncris6r8l6s5l.apps.googleusercontent.com",
-            clientSecret:"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"
+            clientId:"",
+            clientSecret:""
         })
     ]
 })
